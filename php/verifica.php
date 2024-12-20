aea57dc570b7d620707e736de1b87092b924203b
PeQueiroz <pedrohenriquequeiroz130@gmail.com>
2024-12-20 11:01:29 -0300

    Ajustes

--- php/verifica.php
+++ php/verifica.php
@@ -5,15 +5,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['email'], $_POST['senha
     $email = $conn->real_escape_string(trim($_POST['email']));
     $senha = $conn->real_escape_string(trim($_POST['senha']));
 
-    // Consulta ao banco
     $sql = "SELECT * FROM usuario WHERE Email = '$email' AND Senha = '$senha'";
     $result = $connection->query($sql);
 
     if ($result && $result->num_rows > 0) {
-        // Login bem-sucedido
         echo json_encode(["status" => "sucesso", "mensagem" => "Login realizado com sucesso!"]);
     } else {
-        // Credenciais inválidas
         echo json_encode(["status" => "erro", "mensagem" => "Email ou senha incorretos."]);
     }
 } else {
